5f49f314fe25c0558a3cb19736e1df56e0e2ad0d
Rahmir Ford <Rahmir1@Rahmirs-MacBook-Pro.local>
2017-06-09 00:35:35 -0400

    Putting in some work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,11 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 import { VolunteerPage } from '../pages/volunteer/volunteer';
+import { CampaignsPage } from '../pages/campaigns/campaigns';
+import { DonatePage } from '../pages/donate/donate';
+import { OrganizePage } from '../pages/organize/organize';
+import { CommunityPage } from '../pages/community/community';
 
 @Component({
   templateUrl: 'app.html'
@@ -23,8 +26,11 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage },
-      { title: 'Volunteer', component: VolunteerPage }
+      { title: 'Volunteer', component: VolunteerPage },
+      { title: 'Campaigns', component: CampaignsPage },
+      { title: 'Organize', component: OrganizePage },
+      { title: 'Donate', component: DonatePage },
+      { title: 'Community', component: CommunityPage }
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 import {VolunteerPage} from '../pages/volunteer/volunteer';
+import {DonatePage} from '../pages/donate/donate';
+import {CampaignsPage} from '../pages/campaigns/campaigns';
+import { CommunityPage } from '../pages/community/community';
+import { OrganizePage } from '../pages/organize/organize';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,8 +18,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
-    VolunteerPage
+    VolunteerPage,
+    CampaignsPage,
+    DonatePage,
+    OrganizePage,
+    CommunityPage
+
   ],
   imports: [
     BrowserModule,
@@ -25,8 +33,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
-    VolunteerPage
+    VolunteerPage,
+    CampaignsPage,
+    DonatePage,
+    OrganizePage,
+    CommunityPage
   ],
   providers: [
     StatusBar,
